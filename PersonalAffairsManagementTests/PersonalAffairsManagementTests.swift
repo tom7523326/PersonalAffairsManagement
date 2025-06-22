1594d27d191b913e7806d9969f37b05832674c0a
tangshoulin <tangshoulin@outlook.com>
2025-06-23 01:11:11 +0800

    Initial Commit

---
+++ PersonalAffairsManagementTests/PersonalAffairsManagementTests.swift
@@ -0,0 +1,16 @@
+//
+//  PersonalAffairsManagementTests.swift
+//  PersonalAffairsManagementTests
+//
+//  Created by 汤寿麟 on 2025/6/23.
+//
+
+import Testing
+
+struct PersonalAffairsManagementTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
